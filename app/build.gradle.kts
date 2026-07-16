d933f7ac37f0f3109bed2464bf6bea83cbf679f6
galina-ta <galina.terekhona@yandex.ru>
2026-07-16 23:31:28 +0300

    Migrate devServer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,9 @@ kotlin {
             commonWebpackConfig {
                 outputFileName = "robotApp.js"
                 devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
+                    // Serve sources to debug inside browser
+                    static(rootDirPath)
+                    static(projectDirPath)
                 }
             }
         }
